549bba0214ed1dd075dc692372b4a8a450442494
brakkeh <bram.everts7@gmail.com>
2018-02-26 10:28:58 +0100

    bluetooth arduino

---
+++ bluetooth.ino
@@ -0,0 +1,22 @@
+int data;
+void setup() {
+ pinMode(12,OUTPUT);
+
+ Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //if(Serial.available()){
+    data = Serial.read();
+    delay(100);
+    if(data == 1){
+         Serial.println(data);
+    }
+    if(data == 2){
+         Serial.println(data);
+         Serial.println("hello");
+    }
+  
+
+}
